011dd067028cf3e7dca0e6bd04a30a30de0dfb27
SelimMeske <mesic.selim@outlook.com>
2020-06-04 22:47:38 +0200

    version 1.0.0

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 
 app.use(express.static('public'))
+app.use('/sounds', express.static('sounds'));
+app.use('/images', express.static('images'));
 
 
 module.exports = app;
--- public/chat.js
+++ public/chat.js
@@ -1,76 +1,108 @@
-let socket = io();
+window.addEventListener('load', () => {
+    let loader = document.getElementById('loader');
+    loader.style.opacity = 0;
 
-let button = document.getElementById('button');
-let output = document.getElementById('output');
-let message = document.getElementById('message');
-let partner = document.getElementById('partner');
-let idButton = document.getElementById('uniqueId');
-let idField = document.getElementById('idField');
+    /**Remove loader after 1,1sec */
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 1100);
+});
 
-function clearMessageInput() {
-    message.value = '';
-}
+window.addEventListener('DOMContentLoaded', () => {
+    let socket = io();
 
-let userId = '';
+    let button = document.getElementById('button'),
+        output = document.getElementById('output'),
+        message = document.getElementById('message'),
+        partner = document.getElementById('partner'),
+        idButton = document.getElementById('uniqueId'),
+        idField = document.getElementById('idField'),
+        newMsgSound = document.getElementById('newMsg'),
+        startChat = document.getElementById('startChat');
 
-function emitMsg() { socket.emit('chat', {
-    message: message.value,
-    partner: partner.value
-})
-}
+    function clearMessageInput() {
+        message.value = '';
+    }
 
-idButton.addEventListener('click', () => {
+    let userId = '';
 
-    navigator.permissions.query({ name: "clipboard-write" }).then(result => {
-        if (result.state == "granted" || result.state == "prompt") {
-            navigator.clipboard.writeText(userId).then(function () {
-                idButton.innerText = 'ID copied!';
-                idButton.classList.add('bg-warning');
-                setTimeout(()=>{
-                    idButton.innerText = 'Copy ID';
-                    idButton.classList.remove('bg-warning');
-                }, 2000)
-            }, function () {
-                /**on error */
-            });
-        }
+    function emitMsg() {
+        socket.emit('chat', {
+            message: message.value,
+            partner: partner.value
+        })
+    }
+
+    /**Copy ID to clipboard on buttonclick */
+    idButton.addEventListener('click', () => {
+
+        navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+            if (result.state == "granted" || result.state == "prompt") {
+                navigator.clipboard.writeText(userId).then(function () {
+                    idButton.innerText = 'ID copied!';
+                    idButton.classList.add('bg-warning');
+                    setTimeout(() => {
+                        idButton.innerText = 'Copy ID';
+                        idButton.classList.remove('bg-warning');
+                    }, 2000)
+                }, function () {
+                    /**on error */
+                });
+            }
+        });
     });
-});
 
-button.addEventListener('click', () => {
-    let myMsg = message.value;
-    if(!myMsg){
-        return;
-    }
-    emitMsg();
-    clearMessageInput();
-});
+    /**Enter chat on button click */
+    startChat.addEventListener('click', () => {
+        let idGen = document.getElementById('idGen'),
+            idGenGroup = document.getElementById('idGenGroup');
+        idGen.classList.add('startAni');
+        idGenGroup.style.opacity = 0;
+        document.getElementById('main').style.visibility = 'visible';
+    });
 
-document.addEventListener('keypress', (e) => {
-    let myMsg = message.value;
-    if(!myMsg){
-        return
-    }
-    let key = e.which;
-    if(key === 13){
+    /**Send message */
+    button.addEventListener('click', () => {
+        let myMsg = message.value;
+        if (!myMsg) {
+            return;
+        }
         emitMsg();
         clearMessageInput();
-    }
-});
+    });
 
-socket.on('chat', (data) => {
-    
-    output.innerHTML += `<div style="align-self: flex-start" id="chatMsg" class="bg-success"> ${data.message}</div>`
-    output.scrollTop = output.scrollHeight;
-    
-});
+    /**Send message on keypress */
+    document.addEventListener('keypress', (e) => {
+        let myMsg = message.value;
+        if (!myMsg) {
+            return
+        }
+        let key = e.which;
+        if (key === 13) {
+            emitMsg();
+            clearMessageInput();
+        }
+    });
 
-socket.on('mymsg', data => {
-    output.innerHTML += `<div id="chatMsg" class="bg-primary">${data.message}</div>`
-    output.scrollTop = output.scrollHeight;
-});
+    socket.on('chat', (data) => {
+        newMsgSound.play();
+        output.innerHTML += `<div style="align-self: flex-start" id="chatMsg" class="bg-success"> ${data.message}</div>`
+        output.scrollTop = output.scrollHeight;
 
-socket.on('id', (data) => {
-    userId = data;
-    idField.value = data;
+    });
+
+    socket.on('mymsg', data => {
+
+        output.innerHTML += `<div id="chatMsg" class="bg-primary">${data.message}</div>`
+        output.scrollTop = output.scrollHeight;
+    });
+
+    socket.on('id', (data) => {
+        userId = data;
+        idField.value = data;
+    });
+
+    socket.on('broadcast', data => {
+        output.innerHTML += `<i style="color:white;">${data}</i>`
+    });
 });
