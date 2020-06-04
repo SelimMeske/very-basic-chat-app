2a0293f94c050e955a4422fae0ae4063dcfdea8d
SelimMeske <mesic.selim@outlook.com>
2020-06-04 03:04:10 +0200

    added copy to clipboard and more

--- public/chat.js
+++ public/chat.js
@@ -1,35 +1,64 @@
 let socket = io();
 
 let button = document.getElementById('button');
-let handler = document.getElementById('handler');
 let output = document.getElementById('output');
 let message = document.getElementById('message');
 let partner = document.getElementById('partner');
+let idButton = document.getElementById('uniqueId');
+
+function clearMessageInput() {
+    message.value = '';
+}
+
+let userId = '';
 
 function emitMsg() { socket.emit('chat', {
     message: message.value,
-    partner: partner.value,
-    sender: handler.value
+    partner: partner.value
 })
 }
+
+idButton.addEventListener('click', () => {
+
+    navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+        if (result.state == "granted" || result.state == "prompt") {
+            navigator.clipboard.writeText(userId).then(function () {
+                idButton.innerText = 'ID copied!';
+                idButton.classList.add('bg-warning');
+                setTimeout(()=>{
+                    idButton.innerText = 'Copy ID';
+                    idButton.classList.remove('bg-warning');
+                }, 2000)
+            }, function () {
+                /**on error */
+            });
+        }
+    });
+});
+
 button.addEventListener('click', () => {
+    let myMsg = message.value;
+    if(!myMsg){
+        return;
+    }
     emitMsg();
+    clearMessageInput();
 });
 
 document.addEventListener('keypress', (e) => {
+    let myMsg = message.value;
+    if(!myMsg){
+        return
+    }
     let key = e.which;
     if(key === 13){
         emitMsg();
+        clearMessageInput();
     }
 });
 
 socket.on('chat', (data) => {
     
-    if(handler.value === data.sender){
-        output.innerHTML += `<div id="chatMsg" class="bg-primary">${data.message}</div>`
-        output.scrollTop = output.scrollHeight;
-        return
-    }
     output.innerHTML += `<div style="align-self: flex-start" id="chatMsg" class="bg-success"> ${data.message}</div>`
     output.scrollTop = output.scrollHeight;
     
@@ -41,5 +70,5 @@ socket.on('mymsg', data => {
 });
 
 socket.on('id', (data) => {
-    handler.value = data;
+    userId = data;
 });
